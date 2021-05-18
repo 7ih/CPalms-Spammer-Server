3fec889434f99c97bf2c1ce435a62fe04959b66d
7ih <75285668+7ih@users.noreply.github.com>
2021-05-18 17:45:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,33 @@ const app = express();
 const path = require('path');
 const url = require('url');
 const fetch = require('node-fetch');
-
-var loops = {};
+const db = require('pouchdb')('cpalms');
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+function genLink(data) {return `https://cpalms.org/PreviewResource/LikeResourceVideo?id=${data.id}&LikeUnlike=${data.type}`;}
+
+let loops = {};
+function spam(data) {
+  function req() {
+    if (!loops[data.id]) return;
+    fetch(link, {mode: 'no-cors'});
+    setTimeout(req, data.interval);
+  }
+  loops[data.id] = true;
+  const link = genLink(data);
+  console.log(`Spamming ${data.type} requests to ${data.id}`);
+  req();
+}
+
+// restart ID spam after server restart
+db.allDocs({
+  include_docs: true
+}).then(res => {
+  const cpalms = res.rows;
+  for (let i = 0; i < cpalms.length; i++)
+    spam(cpalms[i].doc.data);
+}).catch(err => console.log('database "get" error: ' + err));
 
 const validGoParams = ['id', 'type', 'interval'];
 const validStopParams = ['id'];
@@ -17,22 +40,19 @@ app.get("/go", (req, res) => {
     res.end("Invalid parameters.");
     return;
   }
-  const link = `https://cpalms.org/PreviewResource/LikeResourceVideo?id=${data.id}&LikeUnlike=${data.type}`;
+  const link = genLink(data);
   if (loops[data.id]) {
     res.end(`<a href="${link}" target="_blank">${data.id}</a> is already being spammed`);
     return;
   }
 
-  function request() {
-    if (!loops[data.id]) return;
-    fetch(link, {mode: 'no-cors'});
-    setTimeout(request, data.interval);
-  }
-  
-  loops[data.id] = true;
+  db.put({ 
+    _id: data.id, 
+    data: data 
+  }).catch(err => console.log('database put: ' + err));
+
   res.end(`Spamming ${data.type} requests to <a href="${link}" target="_blank">${data.id}</a>`);
-  console.log(`Spamming ${data.type} requests to ${data.id}`);
-  request();
+  spam(data);
 });
 app.get("/stop", (req, res) => {
   const data = url.parse(req.url,true).query;
@@ -40,11 +60,14 @@ app.get("/stop", (req, res) => {
     res.end("Invalid parameters.");
     return;
   }
-  const link = `https://cpalms.org/PreviewResource/LikeResourceVideo?id=${data.id}`;
-
+  const link = genLink(data);
   if (loops[data.id]) {
     loops[data.id] = false;
-    res.end(`Stopped sending requests to <a href="${link}" target="_blank">${data.id}</a>`);
+    db.get(data.id)
+      .then(cpalms => db.remove(cpalms))
+      .catch(err => console.log('database get/remove: ' + err));
+    res.end(`Stopped spamming <a href="${link}" target="_blank">${data.id}</a>`);
+    console.log(`Stopped spamming ${data.id}`);
   }
   else res.end(`<a href="${link}" target="_blank">${data.id}</a> isn't being spammed`);
 });
@@ -52,3 +75,5 @@ app.get("/stop", (req, res) => {
 app.listen((process.env.PORT || 3000), () => {
   console.log('server started');
 });
+
+// fun fact: i got the whole database system to work first try. pouchdb is AWESOME
