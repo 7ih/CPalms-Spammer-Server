f1e2218861fe86020a73df4f02245f8e52d8c6d0
7ih <75285668+7ih@users.noreply.github.com>
2021-05-21 10:50:05 -0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,25 +1,22 @@
 function $(id) {return document.getElementById(id);}
 function fb(msg) {$("feedback").innerHTML = msg;}
-var type;
 $("stop").addEventListener("click", function() {
-  var linkDirs = $("link").value.split("/");
-  var id = linkDirs[linkDirs.length-1];
+  var id = parseInt($("link").value.match(/\d/g).join(''));
   var params = `?id=${id}`;
   fetch("/stop" + params)
     .then(res => res.text())
     .then(data => fb(data));
 });
 $("go").addEventListener("click", function() {
-  if (!$("link").value.includes("cpalms.org/PreviewResource")) {
-    fb("That isn't a valid cpalms link (i think)");
+  if (!$("link").value.includes("cpalms.org/PreviewResource") && $("link").value.includes("cpalms.org")) {
+    fb("That isn't a valid cpalms tutorial");
     return;
   }
-  var linkDirs = $("link").value.split("/");
-  var id = linkDirs[linkDirs.length-1];
-  type = $("type").value;
+  var id = parseInt($("link").value.match(/\d/g).join(''));
+  var type = $("type").value;
   var interval = parseInt($("interval").value);
   if (!interval || interval < 200) interval = 200;
-
+  
   var params = `?id=${id}&type=${type}&interval=${interval}`;
   fetch("/go" + params)
     .then(res => res.text())
